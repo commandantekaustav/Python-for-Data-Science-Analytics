3b54248439ee2d84681ed4dd1b5592f4d240e792
kaustav <kaustavbasu97@gmail.com>
2025-03-20 12:39:33 +0530

    built

---
+++ dist/assets/md-Cwn7mECe.js
@@ -0,0 +1,11 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-005QRd18.js";import{b as o,o as c,w as e,g as s,e as u,m,B as n,v as d,x as f,C as l}from"./modules/vue-DcZKNubr.js";import{I as _}from"./slidev/default-DITZbOMS.js";import{u as k,f as g}from"./slidev/context-DOn3OHKD.js";import"./modules/unplugin-icons-DbOwnpFF.js";import"./index-DTPcApfY.js";import"./modules/shiki-BYsr0BdZ.js";const N={__name:"virtual_environments_tutorial.md__slidev_673",setup(h){const{$clicksContext:t,$frontmatter:i}=k();return t.setup(),(v,a)=>{const p=r;return c(),o(_,d(f(l(g)(l(i),672))),{default:e(()=>[a[1]||(a[1]=s("h3",null,[s("em",null,"Venv Folder Tree")],-1)),u(p,m({},{ranges:[]}),{default:e(()=>a[0]||(a[0]=[s("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[s("code",{class:"language-text"},[s("span",{class:"line"},[s("span",null,"bash")]),n(`
+`),s("span",{class:"line"},[s("span",null,"project_name/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|-- env/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|   |-- bin/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|   |   |-- python")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|   |   |-- pip")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|   |-- lib/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|   |   |-- python3.9/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|   |   |-- site-packages/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|-- src/")]),n(`
+`),s("span",{class:"line"},[s("span",null,"|   |-- main.py")])])],-1)])),_:1},16)]),_:1},16)}}};export{N as default};
