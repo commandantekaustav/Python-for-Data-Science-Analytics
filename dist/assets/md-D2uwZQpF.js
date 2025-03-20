58b90e91345bbc3b1273736f716f9d80f6c91055
kaustav <kaustavbasu97@gmail.com>
2025-03-20 13:24:08 +0530

    new build

---
+++ dist/assets/md-D2uwZQpF.js
@@ -0,0 +1,15 @@
+import{_ as r}from"./slidev/CodeBlockWrapper.vue_vue_type_script_setup_true_lang-BobOEePV.js";import{b as u,o as d,w as e,g as n,B as s,e as c,m as f,v as m,x as g,C as a}from"./modules/vue-DYRo_Lls.js";import{I as k}from"./slidev/two-cols-header-Bm3rW2CS.js";import{u as v,f as h}from"./slidev/context-Yz-4lzjv.js";import"./modules/unplugin-icons-CJ6nVESg.js";import"./index-Q9xIr-xQ.js";import"./modules/shiki-NVXounY8.js";const _={__name:"session-13.md__slidev_939",setup(y){const{$clicksContext:t,$frontmatter:o}=v();return t.setup(),(D,l)=>{const i=r;return d(),u(k,m(g(a(h)(a(o),938))),{left:e(p=>[l[1]||(l[1]=n("p",null,[n("strong",null,"Output & Explanation (Slide 4):")],-1)),c(i,f({},{ranges:[]}),{default:e(()=>l[0]||(l[0]=[n("pre",{class:"shiki shiki-themes vitesse-dark vitesse-light slidev-code",style:{"--shiki-dark":"#dbd7caee","--shiki-light":"#393a34","--shiki-dark-bg":"#121212","--shiki-light-bg":"#ffffff"}},[n("code",{class:"language-text"},[n("span",{class:"line"},[n("span",null,"Original Data Types:")]),s(`
+`),n("span",{class:"line"},[n("span",null,"ID       int64")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Score    float64")]),s(`
+`),n("span",{class:"line"},[n("span",null,"dtype: object")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Converted DataFrame:")]),s(`
+`),n("span",{class:"line"},[n("span",null,"    ID  Score")]),s(`
+`),n("span",{class:"line"},[n("span",null,"0  1.0     85")]),s(`
+`),n("span",{class:"line"},[n("span",null,"1  2.0     90")]),s(`
+`),n("span",{class:"line"},[n("span",null,"2  3.0     78")]),s(`
+`),n("span",{class:"line"},[n("span")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Converted Data Types:")]),s(`
+`),n("span",{class:"line"},[n("span",null,"ID       float64")]),s(`
+`),n("span",{class:"line"},[n("span",null,"Score      int64")]),s(`
+`),n("span",{class:"line"},[n("span",null,"dtype: object")])])],-1)])),_:1},16)]),right:e(p=>l[2]||(l[2]=[n("p",null,[n("strong",null,"Explanation:")],-1),n("ul",null,[n("li",null,"The 'ID' column is converted from integer to float."),n("li",null,"The 'Score' column is converted from float to integer (truncating the decimal part)."),n("li",null,[s("The "),n("code",null,"df.dtypes"),s(" attribute shows the updated data types after conversion.")])],-1)])),default:e(()=>[l[3]||(l[3]=n("h3",null,"Working with Different Data Types in DataFrames 📊",-1)),l[4]||(l[4]=n("p",null,[n("strong",null,"Data Type Conversion & Manipulation")],-1)),l[5]||(l[5]=n("p",null,"Let’s see the output of converting the data types.",-1))]),_:1},16)}}};export{_ as default};
